6946a0588eddf84753a7d03eea2eb978094017ee
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2025-05-29 16:39:43 +0900

    스프링 프로젝트 생성

---
+++ spring/src/main/java/com/aivle/miniproject3/Miniproject3Application.java
@@ -0,0 +1,13 @@
+package com.aivle.miniproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniproject3Application.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/aivle/miniproject3/Miniproject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivle.miniproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniproject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
